e36043dc7928a98c582a2965a4a3bcbfcef30145
chen <76803356@qq.com>
2023-03-25 09:31:14 +0800

    迁移串口工具实用类

--- app/src/main/java/com/etv/activity/TestActivity.java
+++ app/src/main/java/com/etv/activity/TestActivity.java
@@ -1,24 +1,67 @@
 package com.etv.activity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
 
+import com.etv.util.serialport.SerialPort;
 import com.ys.etv.R;
 
-public class TestActivity extends BaseActivity {
+
+public class TestActivity extends Activity {
+
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        initView();
+        initSeriport();
+        findViewById(R.id.send).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendPortMessage();
+            }
+        });
     }
 
-//    VLCSurfaceView surfaceView_vlc;
 
-    private void initView() {
-//        surfaceView_vlc = (VLCSurfaceView) findViewById(R.id.surfaceView_vlc);
-//        surfaceView_vlc.startPlayUrl("rtsp://192.168.13.6/test");
-//      surfaceView_vlc.startPlayUrl("rtsp://192.168.4.3/test");
+    SerialPort mSerialPort;
+
+    private void initSeriport() {
+        if (mSerialPort == null) {
+            mSerialPort = new SerialPort(this);
+        }
+        mSerialPort.receive(new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(Message msg) {
+                switch (msg.what) {
+                    case SerialPort.RESULT_OK:
+                        Log.e("cdl", "收到消息=" + msg.obj);
+                        break;
+                    case SerialPort.RESULT_ERROR:
+                        Log.e("cdl", "收到消息error=" + msg.obj);
+                        break;
+                }
+                return true;
+            }
+        }));
+    }
+
+    public void sendPortMessage() {
+        String message = "{" + System.currentTimeMillis() + "}";
+        mSerialPort.send(message);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mSerialPort != null) {
+            mSerialPort.release();
+            mSerialPort = null;
+        }
     }
 
 }
---
+++ app/src/main/java/com/etv/util/serialport/SerialPort.java
@@ -0,0 +1,160 @@
+package com.etv.util.serialport;
+
+import android.content.Context;
+import android.os.Handler;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class SerialPort {
+
+    public static final int RESULT_OK = 1001;
+    public static final int RESULT_ERROR = 1002;
+    private static final String TAG = "SerialPort";
+    private Context mContext;
+    private FileDescriptor mFd;
+    private FileInputStream mFileInputStream;
+    private FileOutputStream mFileOutputStream;
+    private static final int PORT_RATE = 115200;
+    private static final File PORT_FILE = new File("/dev/ttyS3");
+
+    public SerialPort(Context context) {
+        mContext = context;
+        if (!PORT_FILE.canRead() || !PORT_FILE.canWrite()) {
+            try {
+                Process su;
+                su = Runtime.getRuntime().exec("/system/bin/su");
+                String cmd = "chmod 777 " + PORT_FILE.getAbsolutePath() + "\n" + "exit\n";
+                su.getOutputStream().write(cmd.getBytes());
+                if ((su.waitFor() != 0) || !PORT_FILE.canRead() || !PORT_FILE.canWrite()) {
+                    Log.w(TAG, "root failed");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        mFd = open(PORT_FILE.getAbsolutePath(), PORT_RATE);
+        if (mFd == null) {
+            Log.w(TAG, "native open returns null");
+        }
+        mFileInputStream = new FileInputStream(mFd);
+        mFileOutputStream = new FileOutputStream(mFd);
+    }
+
+    public void send(String path) {
+        try {
+            byte[] bytes = (path).getBytes("UTF-8");
+            logSerPortMessage(new String(bytes));
+            mFileOutputStream.write(bytes);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void logSerPortMessage(String s) {
+        Log.e("cdl", "logSerPortMessage=" + s);
+    }
+
+    ReadThread mReadThread;
+
+    public void receive(Handler handler) {
+        if (mFileInputStream != null) {
+            if (mReadThread != null) {
+                mReadThread.interrupt();
+                mReadThread = null;
+            }
+            mReadThread = new ReadThread(handler);
+            mReadThread.start();
+        }
+    }
+
+    public void release() {
+        if (mReadThread != null) {
+            mReadThread.interrupt();
+            mReadThread = null;
+        }
+        try {
+            if (mFileOutputStream != null) {
+                mFileOutputStream.close();
+                mFileOutputStream = null;
+            }
+
+            if (mFileInputStream != null) {
+                mFileInputStream.close();
+                mFileInputStream = null;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        close();
+    }
+
+    StringBuilder stringBuilder = new StringBuilder();
+
+    private class ReadThread extends Thread {
+        Handler mHandler;
+
+        public ReadThread(Handler handler) {
+            mHandler = handler;
+        }
+
+        @Override
+        public void interrupt() {
+            super.interrupt();
+            if (mHandler != null) {
+                mHandler = null;
+            }
+        }
+
+        @Override
+        public void run() {
+            super.run();
+            while (!isInterrupted()) {
+                try {
+                    byte[] buffer = new byte[128];
+                    int size = mFileInputStream.read(buffer);
+                    if (size < 1) {
+                        reset();
+                        return;
+                    }
+                    String bufferInfo = new String(buffer, 0, size, "UTF-8");
+                    if (bufferInfo.startsWith("{")) {
+                        logSerPortMessage("开始解析");
+                        stringBuilder.setLength(0);
+                        stringBuilder.append("{");
+                    } else if (bufferInfo.startsWith("}")) {
+                        stringBuilder.append("}");
+                        logSerPortMessage("解析完成=" + stringBuilder.toString());
+                    } else {
+                        logSerPortMessage("解析数据=" + bufferInfo);
+                        stringBuilder.append(bufferInfo);
+                    }
+                } catch (Exception e) {
+                    reset();
+                }
+            }
+        }
+
+        private void reset() {
+            if (mHandler != null) {
+                mHandler.sendEmptyMessage(RESULT_ERROR);
+            }
+        }
+    }
+
+    static {
+        System.loadLibrary("serial_port");
+    }
+
+    public native FileDescriptor open(String path, int baudrate);
+
+    public native void close();
+}
